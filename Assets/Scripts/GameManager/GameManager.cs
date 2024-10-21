39c65db1bce86100e20399d58d054c460aa3bda0
Leo Cunha <leocunha20030@hotmail.com>
2024-10-21 18:35:34 -0300

    Criando repositorio

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private IGameStatus _gameStatus;
+    private void Awake()
+    {
+        ServiceLocator.Reset();
+
+        _gameStatus = new GameStatus();
+        ServiceLocator.RegisterService(_gameStatus);
+        ServiceLocator.RegisterService<ICheckPointSystem>(new CheckPointSystem());
+    }
+
+    void Start()
+    {
+        _gameStatus.OnWinGame += HandleWinGame;
+        _gameStatus.OnGameOver += HandleGameOver;
+    }
+
+    private void HandleWinGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
+    private void HandleGameOver()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/UILifeManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UI;
+using UnityEngine;
+
+public class UILifeManager : MonoBehaviour, IUILifeManager
+{
+    [SerializeField] private UILIfe[] _vetLife;
+    [SerializeField] private Sprite _fullLife;
+    [SerializeField] private Sprite _emptyLife;
+
+    private void Awake()
+    {
+        ServiceLocator.RegisterService<IUILifeManager>(this);
+    }
+
+    public void SetQtdLife(int qtdLife)
+    {
+        int count = _vetLife.Length - qtdLife;
+        count = count > _vetLife.Length ? _vetLife.Length : count;
+        
+        for (int i = 0; i < count; i++)
+        {
+            _vetLife[i].SetImage(_emptyLife);
+        }
+    }
+
+    public void ResetLife()
+    {
+        for (int i = 0; i < _vetLife.Length; i++)
+        {
+            _vetLife[i].SetImage(_fullLife);
+        }
+    }
+}
